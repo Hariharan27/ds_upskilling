2aa62e15f1ebbe9a4921c0852020dc81796ddcc5
Hariharanvickydev <hariharan@ideas2it.com>
2026-06-11 17:10:28 +0530

    Initial project setup

---
+++ employee-helpdesk-ai/app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+from app.core.config import settings
+from app.api.routes.health import router as health_router
+
+app = FastAPI(
+    title=settings.APP_NAME,
+    version=settings.APP_VERSION
+)
+
+app.include_router(
+    health_router,
+    prefix="/api/v1",
+    tags=["Health"]
+)
